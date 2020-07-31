6ee946e8dbd3d989f9f8f75e5ab442eb1da45488
Marcy Sutton <marcy@gatsbyjs.com>
2020-07-31 15:35:13 -0700

    improve access of non-logged-in menu

--- app/assets/javascripts/initializers/initializeTouchDevice.js
+++ app/assets/javascripts/initializers/initializeTouchDevice.js
@@ -26,6 +26,13 @@ function removeShowingMenu() {
 function toggleMenu() {
   getClassList('top-bar__menu').toggle('showing');
 }
+let topBarMenu = getClassList('top-bar__menu');
+
+function handleMenuBtnEscapeKey(e) {
+  if (e.keyCode === 27 && topBarMenu.contains('showing')) {
+    topBarMenu.remove('showing');
+  }
+}
 
 function initializeTouchDevice() {
   var isTouchDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|DEV-Native-ios/i.test(
@@ -40,15 +47,24 @@ function initializeTouchDevice() {
       // Use a named function instead of anonymous so duplicate event handlers are discarded
       getById('navigation-butt').addEventListener('click', toggleMenu);
     } else {
-      getClassList('top-bar__menu').add('desktop');
-      getById('navigation-butt').addEventListener('focus', (e) =>
-        getClassList('top-bar__menu').add('showing'),
-      );
+      topBarMenu.add('desktop');
+      getById('navigation-butt').addEventListener('click', (e) => {
+        if (topBarMenu.contains('showing')) {
+          topBarMenu.remove('showing');
+          getById('navigation-butt').removeEventListener(
+            'keyup',
+            handleMenuBtnEscapeKey,
+          );
+        } else {
+          topBarMenu.add('showing');
+          getById('navigation-butt').addEventListener(
+            'keyup',
+            handleMenuBtnEscapeKey,
+          );
+        }
+      });
       getById('last-nav-link').addEventListener('blur', (e) =>
         blur(e, 'second-last-nav-link'),
-      );
-      getById('navigation-butt').addEventListener('blur', (e) =>
-        blur(e, 'first-nav-link'),
       );
     }
   }, 10);
